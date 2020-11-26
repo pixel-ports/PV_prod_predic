c3c64f0101b21e21a8a0089562bc58462612f9ae
Romain Quéraud <r.queraud@catie.fr>
2020-11-26 18:21:13 +0100

    Simultaneous requests allow to pre-load model

--- WP8/api.py
+++ WP8/api.py
@@ -2,21 +2,62 @@ from flask import Flask
 from flask_restful import Resource, Api
 from subprocess import Popen
 import time
+from training import get_data, split_sequences, MV_LSTM
+from inference import preprocess_data
+import torch
+from torch.autograd import Variable
+
+
+scaler_path = "./trained.scaler"
+model_path = "./trained.model"
+data_path = "./one_sequence.csv"
+
+features = ['Energy_Generated'] # simple LSTM
+n_features = len(features) # this is number of parallel inputs
+n_timesteps = 48 # this is number of timesteps
+
+
+scaler = torch.load(scaler_path)
+
+mv_net_2 = MV_LSTM(n_features,n_timesteps)
+mv_net_2.load_state_dict(torch.load(model_path))
+
+# Loading here is only usefull to get the size of testX to init the hidden layer 
+data = get_data(data_path)
+scaled = preprocess_data(data, features, scaler)
+X, y = split_sequences(scaled, n_timesteps)
+testX = Variable(torch.Tensor(X))
+
+mv_net_2.init_hidden(testX.size(0))
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class inference(Resource):
     def get(self):
-        command = "python3 inference.py ./trained.scaler ./trained.model ./one_sequence.csv"
+        command = "python3 inference.py %s %s %s" % (scaler_path, model_path, data_path)
         begin_time = time.time()
         worker_process = Popen(command.split(" "))
         worker_process.wait()
         worker_process.terminate()
         return time.time() - begin_time
 
+class loaded_inference(Resource):
+    def get(self):  # We reload data to simulate incoming data
+        begin_time = time.time()
+        # data = get_data(data_path)
+        # scaled = preprocess_data(data, features, scaler)
+        # X, _ = split_sequences(scaled, n_timesteps)
+        # testX = Variable(torch.Tensor(X))
+        result = mv_net_2(testX).detach().numpy()
+        return time.time() - begin_time
+
+
 api.add_resource(inference, '/')
+api.add_resource(loaded_inference, '/loaded_inference/')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True)
     
--- WP8/simultaneous_requests.py
+++ WP8/simultaneous_requests.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import argparse
 from subprocess import Popen
 
-def parallel_works(nb_processes):
+def parallel_works(nb_processes, api_path):
     # command = ["xargs", "-I", "%", "-P", "10000", "curl http://localhost:5000", "< <(printf '%s\n' {1..3})"]
-    command = "xargs -I % -P 8 curl 'http://localhost:5000' < <(printf '%s\n' {1.."+str(nb_processes)+"})"
+    command = "xargs -I % -P 1000 curl 'http://localhost:5000"+api_path+"' < <(printf '%s\n' {1.."+str(nb_processes)+"})"
     begin_time = time.time()
     worker_process = Popen(["bash", "-c", command])
     worker_process.wait()
@@ -13,18 +13,20 @@ def parallel_works(nb_processes):
     return time.time() - begin_time
 
 
-def main(min_processes, max_processes, step_processes):
+def main(min_processes, max_processes, step_processes, api_path):
     x, y = [], []
     for nb_processes in range(min_processes, max_processes, step_processes):
         print("Test with %d processes." % nb_processes)
+        total_time = parallel_works(nb_processes, api_path)
         x.append(nb_processes)
-        y.append(round(parallel_works(nb_processes), 2))
-        print("  ==> %f" % y[-1])
+        y.append(round(total_time, 2))
+        print("total_time ==> %f" % y[-1])
 
     print(x)
     print(y)
+
     plt.xlabel("Number of process")
-    plt.ylabel("Time (ms)")
+    plt.ylabel("Time (s)")
     plt.plot(x, y)
     plt.show()
 
@@ -44,6 +46,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--step_processes", type=int, help="Step number of processes to increment."
     )
+    parser.add_argument(
+        "api_path", type=str, help="Path to call in the running api: e.g. / or /loaded_inference/"
+    )
     args = parser.parse_args()
 
-    main(args.min_processes, args.max_processes, args.step_processes)
+    main(args.min_processes, args.max_processes, args.step_processes, args.api_path)
